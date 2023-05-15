183556842efef649b09145715d0b355318e67bc4
admin <admin@gmail.com>
2023-05-15 20:11:25 +0530

    first commit

---
+++ src/components/MainForm.tsx
@@ -0,0 +1,75 @@
+import React, {useState} from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
+import "./CommonCss.scss";
+import ChildForm from './ChildForm';
+
+type UserSubmitForm = {
+  fullname: string;
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  acceptTerms: boolean;
+};
+
+const MainForm = () => {
+
+    const [formdata, setFormdata] = useState<UserSubmitForm>({
+        fullname: '',
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+        acceptTerms: false
+      });
+
+    // const [formdata1, setFormdata1] = useState<{}>({})
+
+    const validationSchema = Yup.object().shape({
+        fullname: Yup.string().required('Fullname is required'),
+        username: Yup.string()
+          .required('Username is required')
+          .min(6, 'Username must be at least 6 characters')
+          .max(20, 'Username must not exceed 20 characters'),
+        email: Yup.string()
+          .required('Email is required')
+          .email('Email is invalid'),
+        password: Yup.string()
+          .required('Password is required')
+          .min(6, 'Password must be at least 6 characters')
+          .max(40, 'Password must not exceed 40 characters'),
+        confirmPassword: Yup.string()
+          .required('Confirm Password is required'),
+        //   .oneOf([Yup.ref('password'), null], 'Confirm Password does not match'),
+        acceptTerms: Yup.bool().oneOf([true], 'Accept Terms is required')
+      });
+    
+      const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors }
+      } = useForm<UserSubmitForm>({
+        resolver: yupResolver(validationSchema)
+      });
+    
+      const onSubmit = (data: UserSubmitForm) => {
+        console.log(JSON.stringify(data, null, 2));
+      };
+
+    return (
+        <>
+        Main
+        <ChildForm
+        {...formdata}
+        // setFormdata={setFormdata}
+        setLoginStatus={setFormdata}
+        />
+        </>
+    )
+
+}
+
+export default MainForm;
